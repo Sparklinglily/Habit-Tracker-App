205c171512cda7573687df721bb1e44e3eb8f930
Sparklinglily <carlaejemeh@gmail.com>
2024-01-16 21:16:57 +0100

    jode

--- lib/presentation/views/authentication/checkEmail.dart
+++ lib/presentation/views/authentication/checkEmail.dart
@@ -6,23 +6,20 @@ import '../../../core/constants/constants.dart';
 
 class CheckEmail extends StatefulWidget {
   const CheckEmail({Key? key, required this.email}) : super(key: key);
-final String email;
+  final String email;
   @override
   State<CheckEmail> createState() => _CheckEmailState();
 }
 
 class _CheckEmailState extends State<CheckEmail> {
-
-  void onPressed(){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginPage()));
+  void onPressed() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const LoginPage()));
   }
 
-  void onPressed2(context){
-    context.read<FirebaseAuthMethods>().passwordReset(widget.email,context);
-
+  void onPressed2(context) {
+    context.read<FirebaseAuthMethods>().passwordReset(widget.email, context);
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -32,45 +29,41 @@ class _CheckEmailState extends State<CheckEmail> {
           Column(
             children: [
               Container(
-                height:  200,
+                height: 200,
                 width: double.infinity,
                 decoration: const BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage("images/welcome.png"),
-                        fit: BoxFit.cover
-                    )
-                ),
+                        fit: BoxFit.cover)),
               ),
-
             ],
           ),
 
           Container(
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text("Check your mail",
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Check your mail",
                   style: TextStyle(
                     fontSize: defaultSpacing * 2,
-
-                  ),),
-                  const SizedBox(height: defaultSpacing),
-
-                  Text("We sent a reset password link \n to "
-                      "${widget.email}", style: const TextStyle(
-                    fontSize: defaultSpacing * 1.2
-                  ),)
-                ],
-              ),
+                  ),
+                ),
+                const SizedBox(height: defaultSpacing),
+                Text(
+                  "We sent a reset password link \n to "
+                  "${widget.email}",
+                  style: const TextStyle(fontSize: defaultSpacing * 1.2),
+                )
+              ],
+            ),
           ),
           const SizedBox(height: defaultSpacing * 2),
 
-
           //login button
           Padding(
-            padding: const EdgeInsets.all(28.0),
+            padding: const EdgeInsets.all(32.0),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: primaryLight,
@@ -79,11 +72,15 @@ class _CheckEmailState extends State<CheckEmail> {
                   borderRadius: BorderRadius.circular(16),
                 ),
               ),
-              onPressed: (){onPressed();},
+              onPressed: () {
+                onPressed();
+              },
               child: const Text(
-                "Log in", style: TextStyle(
-                  color: background,
-                  fontWeight: FontWeight.bold, fontSize: defaultSpacing * 1.2),
+                "Log in",
+                style: TextStyle(
+                    color: background,
+                    fontWeight: FontWeight.bold,
+                    fontSize: defaultSpacing * 1.2),
               ),
             ),
           ),
@@ -93,18 +90,17 @@ class _CheckEmailState extends State<CheckEmail> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("Didn't receive mail?"),
-              TextButton(onPressed: (){onPressed2(context);}, child: const Text("Click to Resend",))
+              TextButton(
+                  onPressed: () {
+                    onPressed2(context);
+                  },
+                  child: const Text(
+                    "Click to Resend",
+                  ))
             ],
           )
-
-
-
-
-
         ],
       ),
-
-
     );
   }
 }
