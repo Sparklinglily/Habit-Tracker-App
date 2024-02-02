ecba26c74faabe5935d213ff4e3dec02e966eae5
Sparklinglily <carlaejemeh@gmail.com>
2024-02-02 21:24:14 +0100

    fixes

--- lib/presentation/views/addHabit/addHabitPage.dart
+++ lib/presentation/views/addHabit/addHabitPage.dart
@@ -63,7 +63,7 @@ class _AddHabitPageState extends State<AddHabitPage> {
                     hintText: 'duration of habit'),
               ),
               const SizedBox(
-                height: 16,
+                height: 22,
               ),
               ElevatedButton.icon(
                   onPressed: () async {
