4b1f4b66140609f30c8b4c89081fea63364cd03d
Sparklinglily <carlaejemeh@gmail.com>
2024-02-16 22:40:49 +0100

    favorites page

--- lib/presentation/views/Favorites/favorites.dart
+++ lib/presentation/views/Favorites/favorites.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:habit_help/presentation/styles/constants.dart';
+import 'package:habit_help/presentation/views/Favorites/grid_items.dart';
 
 class Favorites extends StatelessWidget {
-  const Favorites({super.key});
+  final List<GridItemData> gridItemData = [
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 239, 236, 231),
+        image: ('images/plateOfFruits.jpg'),
+        miniTitle: '20 people',
+        title: 'Fruity 30 days'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 245, 232, 246),
+        image: ('images/trackRun.jpg'),
+        miniTitle: 'Run 1km',
+        title: '12 people'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 230, 238, 244),
+        image: ('images/yoga2.jpeg'),
+        miniTitle: '10 people',
+        title: 'Lets do yoga'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 248, 247, 230),
+        image: ('images/mediatation.png'),
+        miniTitle: '8 people',
+        title: '30 days meditation'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 238, 238, 248),
+        image: ('images/readingAbOOK.png'),
+        miniTitle: '10 people',
+        title: 'Readers are leaders'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 243, 251, 244),
+        image: ('images/water.png'),
+        miniTitle: '8 people',
+        title: 'Drink water'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 248, 247, 230),
+        image: ('images/mediatation.png'),
+        miniTitle: '8 people',
+        title: '30 days meditation'),
+    GridItemData(
+        onTapped: () {},
+        color: const Color.fromARGB(255, 248, 247, 230),
+        image: ('images/mediatation.png'),
+        miniTitle: '8 people',
+        title: '30 days meditation'),
+  ];
+
+  Favorites({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Column(
+          children: [
+            TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Close',
+                  style: TextStyle(color: primaryLight, fontSize: 16),
+                )),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: GridView.builder(
+                    scrollDirection: Axis.vertical,
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 15.0,
+                      childAspectRatio: 16 / 17.5,
+                    ),
+                    itemCount: gridItemData.length,
+                    itemBuilder: (context, index) {
+                      return GridItem(
+                        gridData: gridItemData[index],
+                      );
+                    }))
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/presentation/views/Favorites/grid_items.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class GridItemData {
+  final String image;
+  final String title;
+  final String miniTitle;
+  final Color color;
+  final Function()? onTapped;
+
+  GridItemData(
+      {required this.onTapped,
+      required this.color,
+      required this.image,
+      required this.miniTitle,
+      required this.title});
+}
+
+class GridItem extends StatelessWidget {
+  const GridItem({required this.gridData, super.key});
+  final GridItemData gridData;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Card(
+        child: Column(
+      children: [
+        Stack(children: [
+          Container(
+              height: size.height * 2,
+              child: Image.asset(
+                gridData.image,
+                fit: BoxFit.contain,
+              )),
+          Positioned(
+              left: 16,
+              top: 16,
+              child: InkWell(
+                onTap: gridData.onTapped,
+                child: Container(
+                  padding: EdgeInsets.all(6),
+                  child: Icon(Icons.delete),
+                ),
+              ))
+        ]),
+        Container(
+          decoration: BoxDecoration(
+            color: gridData.color,
+          ),
+          child: Column(children: [
+            Text(gridData.title),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [Icon(Icons.group), Text(gridData.miniTitle)],
+              ),
+            )
+          ]),
+        )
+      ],
+    ));
+  }
+}
