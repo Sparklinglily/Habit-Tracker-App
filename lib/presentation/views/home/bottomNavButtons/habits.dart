fa9db5478f1a5cdf92558573f83c2926c27ba444
Sparklinglily <carlaejemeh@gmail.com>
2024-02-23 22:35:29 +0100

    habit page

--- lib/presentation/views/home/bottomNavButtons/habits.dart
+++ lib/presentation/views/home/bottomNavButtons/habits.dart
@@ -5,6 +5,10 @@ class HabitsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome on here'),
+      ),
+    );
   }
 }
