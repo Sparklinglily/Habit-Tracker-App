e92fea234d1bc3fba6812bd3a21e37f55e5badb0
Sparklinglily <carlaejemeh@gmail.com>
2024-03-27 21:15:01 +0100

    minor fix

--- lib/presentation/views/MainScreens/bottomNavButtons/Habit/habits.dart
+++ lib/presentation/views/MainScreens/bottomNavButtons/Habit/habits.dart
@@ -32,7 +32,7 @@ class HabitsPage extends StatelessWidget {
                           color: fontDark,
                           height: 1.5,
                           fontWeight: FontWeight.w500,
-                          fontSize: 24),
+                          fontSize: 25),
                     )
                   ],
                   indicatorColor: primaryLight,
